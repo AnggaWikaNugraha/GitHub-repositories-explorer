0a33899233be953712cb2f88956e3d9fe7684ab7
AnggaWikaNugraha <anggawika18@gmail.com>
2025-08-05 09:41:41 +0700

    fix: implementasi reach hook form

--- src/pages/githubExplorePage/_components/searchBar/index.tsx
+++ src/pages/githubExplorePage/_components/searchBar/index.tsx
@@ -6,6 +6,11 @@ import UserItem from "../../_components/userItem";
 import { useDispatch, useSelector } from 'react-redux';
 import { AppDispatch, RootState } from '../../../../store/store';
 import { setInput, setQuery } from '../../../../store/feature/searchSlice';
+import { useForm } from 'react-hook-form';
+
+type SearchForm = {
+  input: string;
+};
 
 const SearchBar = () => {
 
@@ -14,12 +19,19 @@ const SearchBar = () => {
   const query = useSelector((state: RootState) => state.search.query);
   const { users, loading, error } = useSearch(query);
 
-  const handleSearch = () => {
-    if (input.trim()) dispatch(setQuery(input.trim()));
-  };
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    formState: { errors },
+  } = useForm<SearchForm>();
 
-  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
-    if (e.key === "Enter") handleSearch();
+  const onSubmit = (data: SearchForm) => {
+    const trimmed = data.input.trim();
+    if (trimmed) {
+      dispatch(setInput(trimmed));
+      dispatch(setQuery(trimmed));
+    }
   };
 
   return (
@@ -27,20 +39,23 @@ const SearchBar = () => {
       <div className="flex flex-col gap-4 bg-white p-6 w-full max-w-sm mx-auto h-100">
 
         <p className='text-xs'>Created by @AnggaWIkaNugraha</p>
-        <input
-          type="text"
-          placeholder="Enter username"
-          value={input}
-          onChange={(e) => dispatch(setInput(e.target.value))}
-          onKeyDown={handleKeyDown}
-          className="border border-gray-300 rounded px-4 py-2 w-full focus:outline-none focus:ring-2 focus:ring-blue-400"
-        />
-        <button
-          onClick={handleSearch}
-          className="bg-blue-500 text-white py-2 rounded hover:bg-blue-600 w-full"
-        >
-          Search
-        </button>
+        <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-4">
+          <input
+            type="text"
+            placeholder="Enter username"
+            {...register("input", { required: "Username is required" })}
+            className="border border-gray-300 rounded px-4 py-2 w-full focus:outline-none focus:ring-2 focus:ring-blue-400"
+          />
+          {errors.input && <p className="text-red-500 text-xs">{errors.input.message}</p>}
+
+          <button
+            type="submit"
+            className="bg-blue-500 text-white py-2 rounded hover:bg-blue-600 w-full"
+          >
+            Search
+          </button>
+        </form>
+
 
         {loading && <p className="text-sm text-gray-500 mt-2">Loading...</p>}
         {error && <p className="text-sm text-red-500 mt-2">{error}</p>}
